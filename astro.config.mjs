5c441b9cd41e3b9218a704daad3e74d2403ce66e
Konrad Kubczyk <konrad@kubczyk.com>
2023-10-19 13:33:02 +0200

    Add Svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,19 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import sitemap from "@astrojs/sitemap";
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://konradkubczyk.com/",
   integrations: [
     tailwind(),
-    sitemap()
+    sitemap(),
+    svelte()
   ],
   outDir: "public",
   publicDir: "static",
   server: {
-    port: 3000,
+    port: 3000
   }
 });

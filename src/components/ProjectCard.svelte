0e5fbe37b74849f4eae4f80fc287f61a684820b0
Konrad Kubczyk <konrad@kubczyk.com>
2024-01-01 21:15:17 +0100

    refactor: extract various components from main content

---
+++ src/components/ProjectCard.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    export let name: string;
+    export let description: string;
+    export let tags: string[];
+    export let link: string;
+</script>
+
+<a
+        class="project-card flex-1 flex rounded-lg border border-zinc-600/50 p-5 flex-col gap-3 cursor-pointer bg-neutral-800/25 transition hover:bg-neutral-700/25 justify-between"
+        href={link}
+        target="_blank"
+>
+    <div>
+        <h3 class="text-2xl font-semibold text-neutral-100">{name}</h3>
+        <p class="text-white/70 mt-3">
+            {description}
+        </p>
+    </div>
+    <div
+            class="flex text-xs text-neutral-100 uppercase gap-2 tracking-wider border-t border-zinc-600/50 pt-5 mt-1 flex-wrap sm:flex-nowrap"
+    >
+        {#each tags as tag}
+            <span class="tag border border-white/10 px-2 py-1 rounded whitespace-nowrap font-semibold text-white/70">
+                    {tag}
+                </span>
+        {/each}
+    </div>
+</a>
---
+++ src/components/Projects.svelte
@@ -0,0 +1,91 @@
+<script lang="ts">
+    import ProjectCard from "./ProjectCard.svelte";
+
+    interface Project {
+        name: string;
+        description: string;
+        tags: string[];
+        link: string;
+    }
+
+    export let projects: Project[];
+
+    let tags: Set<string> = new Set([...projects.map(project => project.tags)].flat());
+    let activeFilters: Set<string> = new Set();
+
+    function toggleFilter(this: HTMLElement) {
+        const filter = this.innerHTML.trim();
+        if (!activeFilters.has(filter)) {
+            activeFilters = new Set([...activeFilters, filter]);
+        } else {
+            activeFilters = new Set([...activeFilters].filter(item => item !== filter));
+        }
+    }
+
+    function matchesFilters(tags: string[], filters: Set<string>, mode: "all" | "any" = "all") {
+        const filtersArray = Array.from(filters);
+        if (mode === "all") {
+            return filtersArray.every(tag => tags.includes(tag));
+        } else {
+            return filtersArray.some(tag => tags.includes(tag));
+        }
+    }
+</script>
+
+<div
+        id="portfolio"
+        class="flex gap-5 flex-col xl:flex-row justify-between"
+>
+    <div class="flex flex-col justify-end">
+        <div class="flex flex-col-reverse">
+            <h2 class="text-3xl sm:text-4xl font-semibold text-neutral-100">
+                Portfolio
+            </h2>
+            <span
+                    class="text-sm uppercase font-semibold tracking-widest text-neutral-100/50 mb-2"
+            >
+              My projects
+            </span>
+        </div>
+    </div>
+    <div
+            class="flex gap-2 flex-wrap xl:flex-wrap-reverse xl:justify-end xl:items-start"
+    >
+        {#each tags as tag}
+            <button
+                    class="flex items-center text-center uppercase font-semibold tracking-wide text-sm bg-neutral-800/25 px-3 py-2 rounded-full border border-zinc-600/50 transition duration-200 text-neutral-100/75 hover:bg-neutral-700/25 cursor-pointer select-none whitespace-nowrap"
+                    on:click={toggleFilter}
+                    style={activeFilters.has(tag) ? "background: white; border-color: white; color: black;" : ""}
+            >
+                {tag}
+            </button>
+        {/each}
+    </div>
+</div>
+<div id="projects" class="mt-5 flex gap-5 flex-col sm:flex-row flex-wrap">
+    {#each projects as project}
+        {#if matchesFilters(project.tags, activeFilters)}
+            <ProjectCard
+                    name={project.name}
+                    description={project.description}
+                    tags={project.tags}
+                    link={project.link}
+            />
+        {/if}
+    {/each}
+    {#if activeFilters.size > 0 && projects.filter(project => matchesFilters(project.tags, activeFilters)).length === 0}
+        <p
+                id="no-projects-for-criteria-alert"
+                class="text-white/70 mt-5"
+        >
+            No projects match the selected criteria.
+            <button
+                    class="underline hover:text-white transition"
+                    on:click={() => activeFilters = new Set()}
+            >
+                Click here
+            </button>
+            to reset the filters.
+        </p>
+    {/if}
+</div>

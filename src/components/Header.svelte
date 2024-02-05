d8a28ae235ffa9821e5c10e0fff52c59ccfa8f6a
Konrad Kubczyk <konrad@kubczyk.com>
2024-02-05 15:22:49 +0100

    feat: swap icons

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -54,30 +54,9 @@
                 <button id="menu-button" class="sm:hidden text-white/75 hover:text-white transition cursor-pointer mx-1"
                         aria-label="Menu" on:click={() => toggleMenu()}>
                     {#if isMenuOpen}
-                        <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                height="1.5em"
-                                class="fill-current"
-                                viewBox="0 0 384 512"
-                        >
-                            <!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
-                            <path
-                                    d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"
-                            />
-                        </svg>
+                        <i class="bi bi-x-lg text-lg"></i>
                     {:else}
-                        <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                height="1.2em"
-                                viewBox="0 0 448 512"
-                                class="fill-current"
-                        >
-                            <!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
-                            <path
-                                    d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"
-                            />
-                        </svg
-                        >
+                        <i class="bi bi-list text-xl"></i>
                     {/if}
                 </button>
             </div>

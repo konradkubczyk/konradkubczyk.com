33929993fb4acc87218b3310a5cfee411397110d
Konrad Kubczyk <konrad@kubczyk.com>
2025-11-11 17:09:34 +0000

    refactor: move the hero section to a dedicated component

--- app/page.tsx
+++ app/page.tsx
@@ -1,60 +1,27 @@
 "use client";
 
-import Image from "next/image";
 import { useRef } from "react";
 import { Experience } from "@/app/_components/Experience";
-import { Featured } from "@/app/_components/Featured";
 import { Header } from "@/app/_components/Header/Header";
 import { Skills } from "@/app/_components/Skills";
-import { outfit } from "@/app/fonts";
-import background from "../public/background.webp";
+import { Hero } from "@/app/_components/Hero";
 
 const Home = () => {
-  const hero = useRef(null);
+  const heroSectionRef = useRef(null);
 
   return (
     <>
-      <Header observableRef={hero} />
+      <Header observableRef={heroSectionRef} />
 
       <main className="flex flex-col gap-y-32">
         <section
-          ref={hero}
+          ref={heroSectionRef}
           className="relative flex flex-col"
           style={{
             minHeight: "85vh",
           }}
         >
-          <Image
-            alt=""
-            src={background}
-            fill
-            quality={100}
-            sizes="100vw"
-            className="object-cover opacity-40 invert dark:opacity-60 dark:invert-0"
-            placeholder="blur"
-          />
-          <div className="z-10 flex flex-1 flex-col items-center justify-between gap-20 bg-gradient-to-b from-neutral-200/40 from-80% to-neutral-200 dark:from-neutral-950/75 dark:to-neutral-950">
-            <div className="h-14"></div>
-            <div className="grid w-full max-w-screen-xl gap-5 px-5 md:grid-cols-2">
-              <div className="flex flex-col gap-5">
-                <h1
-                  className={`${outfit.className} ml-[-.25rem] text-6xl font-light tracking-tight text-neutral-800 dark:text-neutral-300`}
-                >
-                  Konrad Kubczyk
-                </h1>
-                <p>
-                  I am a creative, open-minded and motivated person. My
-                  experience in web and software development is complemented by
-                  an eye for design and eagerness to innovate while solving
-                  problems.
-                </p>
-              </div>
-              <div></div>
-            </div>
-            <div className="grid max-w-screen-xl gap-5 px-5 sm:grid-cols-3">
-              <Featured />
-            </div>
-          </div>
+          <Hero />
         </section>
 
         <section

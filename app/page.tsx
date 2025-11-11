eb8cb65fdc3dc69a8e689683653b5a242dbf92fe
Konrad Kubczyk <konrad@kubczyk.com>
2025-11-11 18:35:19 +0000

    chore: configure dark mode support

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,16 @@
 import Image from "next/image";
 
 const Home = () => (
-  <div className="flex h-dvh items-center justify-center">
+  <div className="flex h-dvh flex-col items-center justify-center gap-10">
     <Image
       className="dark:invert"
       src="/logo.svg"
       alt="Logo"
       width={100}
-      height={20}
+      height={100}
       priority
     />
+    <h1 className="text-muted-foreground font-sans text-2xl">Konrad Kubczyk</h1>
   </div>
 );
 
